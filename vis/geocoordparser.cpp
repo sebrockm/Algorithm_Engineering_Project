b143676e579d8d9a268810cbd4620fcac6b81cef
Sebastian Brockmeyer <sebrockm@uos.de>
2013-11-16 19:44:45 +0100

    kugelkoordinaten --> check

--- vis/geocoordparser.cpp
+++ vis/geocoordparser.cpp
@@ -1,5 +1,7 @@
 #include <iostream>
 #include <fstream>
+#include <cmath>
+#include <cstdlib>
 
 int main(int argc, char** argv)
 {
@@ -18,10 +20,22 @@ int main(int argc, char** argv)
 		if(input>>name>>gn>>b>>mn>>b>>sn>>b>>b
 				>>go>>b>>mo>>b>>so>>b>>b)
 		{
-			int x = go*60+mo;
-			int y = gn*60+mn;
+			double phi = -10 + go + mo/60.0 + so/3600.0;
+			double theta = -50 + gn + mn/60.0 + sn/3600.0;
+			theta = 90.0 - theta;
+			double r = 637.1;
+
+			const double PI = 3.14159265359;
+
+			phi *= PI/180;
+			theta *= PI/180;
+
+			int z = (int)(r*sin(theta)*cos(phi));
+			int x = (int)(r*sin(theta)*sin(phi));
+			int y = (int)(r*cos(theta));
+
 			output <<  x << " " << y << " "
-				<< std::max(15,so) << " " << std::max(10,sn) << " "
+				<< 2*name.size() << " " << rand()%2+1 << " "
 				<< name << " 1 " << x << " " << y
 				<< std::endl;
 		}
