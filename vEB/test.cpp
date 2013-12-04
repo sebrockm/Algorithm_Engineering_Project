3be4488a2996e821418a9640207bf189e8581be5
sebrockm <sebrockm@goyle.(none)>
2013-12-04 17:34:57 +0100

    folien bsp verifiziert

--- vEB/test.cpp
+++ vEB/test.cpp
@@ -12,17 +12,17 @@ using namespace std;
 int main()
 {
 	srand(time(0));
-	const unsigned H = 22;
+	const unsigned H = 4;
 	const unsigned long long N = (1ull << H) - 1;
 	VEB<int, H> veb;
 	cout << "N=" << N << " sizeof(veb)/sizeof(int)=" << sizeof(veb)/sizeof(int) << endl;
 
-	vector<int> v;
+	vector<int> v = {52,27,75,14,7,21,40,34,46,63,58,70,89,81,94};
 
-	for(unsigned i=0; i<N; i++)
+	/*for(unsigned i=0; i<N; i++)
 	{
 		v.push_back(rand());
-	}
+	}*/
 
 	sort(v.begin(), v.end());
 
@@ -43,6 +43,9 @@ int main()
 			cout << "BEHHHH" << endl;
 	}
 
-	cout << "0 ist " << (veb.search(0)?"":"nicht") << " drin, veb[0]=" << veb[0] << endl;
+	//cout << "0 ist " << (veb.search(0)?"":"nicht") << " drin, veb[0]=" << veb[0] << endl;
 
+        int* p = (int*)&veb;
+        for(unsigned i=0; i<N; i++)
+            cout << p[i] << ",";
 }
